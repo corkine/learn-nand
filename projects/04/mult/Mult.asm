1272799f73b2b3255e3ee8396cb32648a628778a
corkine <corkine@outlook.com>
2022-06-12 14:25:33 +0800

    finish chapter04 learn

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -10,3 +10,30 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+@res
+M=0
+@R0
+D=M
+@LOOP_END
+D;JEQ
+@R1
+D=M
+@LOOP_END
+D;JEQ
+(LOOP)  
+    @LOOP_END
+    D;JLE
+    @R0
+    D=M
+    @res
+    M=D+M
+    @R1
+    M=M-1
+    D=M
+    @LOOP
+    0;JMP
+(LOOP_END)
+@res
+D=M
+@R2
+M=D
