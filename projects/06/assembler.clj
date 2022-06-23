fd019e9f23bda7e052b7d3f5d5de2032f8fdbcd7
mazj <mazhangjing@inspur.com>
2022-06-23 10:25:55 +0800

    update chapter06 learn

--- projects/06/assembler.clj
+++ projects/06/assembler.clj
@@ -2,62 +2,26 @@
   (:require [clojure.string :as str]
             [clojure.java.io :as io]))
 
-(defn is-a
-  "是否是数据命令，@ 开头后跟符号或十进制"
-  [command]
-  (or (re-find #"^@[a-zA-Z0-9\-\+\&\|\!_\.]+$" command)
-      (re-find #"^@\d+$" command)))
+(def is-a #(or (re-find #"^@[a-zA-Z0-9\-\+\&\|\!_\.]+$" %)
+               (re-find #"^@\d+$" %)))
+(def get-variable #(get (re-find #"^@([a-zA-Z].*?)$" %) 1 nil))
 
-(defn get-variable
-  "从输入中提取数据命令 @ 开头后的符号作为变量，从 16 开始分配
-   允许 ab.c、SP 这样的形式"
-  [command]
-  (get (re-find #"^@([a-zA-Z].*?)$" command) 1 nil))
+(def is-c #(let [find (re-find #"\w?=?\w{0,3};?\w{0,3}?" %)]
+             (and (not (nil? find)) (not (str/blank? find)))))
+(def is-c-jmp #(str/includes? % ";"))
 
-(defn is-c
-  "是否是指令命令，dest=comp;jump 形式, dest 和 jump 都可为空
-   其中 dest 为空省略 =，jump 为空省略 ;"
-  [command]
-  (let [find (re-find #"\w?=?\w{0,3};?\w{0,3}?" command)]
-    (and (not (nil? find))
-         (not (str/blank? find)))))
+(def is-l #(not (nil? (re-find #"\([a-zA-Z0-9\$\._-]+\)" %))))
+(def get-symbol-from-l #(get (re-find #"\(([a-zA-Z0-9\$\._-]+)\)" %) 1))
+(def get-symbol #(get (re-find #"^@([a-zA-Z0-9]+)$" %) 1))
+(def get-dest #(get (re-find #"(\w+)?=\w{0,3};?\w{0,3}?" %) 1 nil))
+(def get-comp
+  #(get (re-find (if (str/includes? % ";")
+                   #"(\w+);\w+" #"\w+=([a-zA-Z0-9\-\+\&\|\!]+)") %) 1 nil))
+(def get-jump #(get (re-find #"\w?=?\w{0,3};?(\w{0,3})?" %) 1 nil))
 
-(defn is-c-jmp
-  "是否是指令命令中的跳转"
-  [command]
-  (str/includes? command ";"))
-
-(defn is-l
-  "是否是伪命令，即 (xxx), 其中 xxx 为符号"
-  [command]
-  (not (nil? (re-find #"\([a-zA-Z0-9\$\._-]+\)" command))))
-
-(defn get-symbol-from-l
-  "从伪命令获取符号，即 (xxx), 其中 xxx 为符号"
-  [command]
-  (get (re-find #"\(([a-zA-Z0-9\$\._-]+)\)" command) 1))
-
-(defn get-symbol
-  "返回 A 命令的符号或数据"
-  [command]
-  (get (re-find #"^@([a-zA-Z0-9]+)$" command) 1))
-
-(defn get-dest
-  "返回 D 命令的目的地址 D=D+A D;JMP"
-  [command]
-  (get (re-find #"(\w+)?=\w{0,3};?\w{0,3}?" command) 1 nil))
-
-(defn get-comp
-  "返回 D 命令的比较助记符 D=D+A D;JMP"
-  [command]
-  (get (re-find (if (str/includes? command ";")
-                  #"(\w+);\w+"
-                  #"\w+=([a-zA-Z0-9\-\+\&\|\!]+)") command) 1 nil))
-
-(defn get-jump
-  "返回 D 命令的跳转助记符"
-  [command]
-  (get (re-find #"\w?=?\w{0,3};?(\w{0,3})?" command) 1 nil))
+(def imme-num->bin
+  #(str/replace
+    (format "%15s" (Integer/toBinaryString (Integer/parseInt %))) " " "0"))
 
 (def comp-map
   "comp 符号表，7 位，包括 111a c1-6"
@@ -135,42 +99,27 @@
    "SCREEN" 16384
    "KBD" 24576})
 
-(defn imme-num->bin [numstr]
-  (str/replace
-   (format "%15s" (Integer/toBinaryString (Integer/parseInt numstr)))
-   " " "0"))
-
-(defn collector
-  "对于输入的每行进行命令映射"
-  [output new-command]
-  (let [_ (println "Handling" new-command)
-        new-command (str/trim new-command)]
-    (cond (str/blank? new-command) output
-          (str/starts-with? new-command "//") output
-          ;如果是 a
-          ;如果是数字，则将十进制数字取出来转换为 16 位的数字
-          ;如果是符号，则..
-          (is-a new-command)
-          (let [numstr (get-symbol new-command)]
-            (conj output (str "0" (imme-num->bin numstr))))
-          ;如果是 c，则提取 dest、comp 和 jump
-          (is-c new-command)
-          (let [dest (get-dest new-command)
-                comp (get-comp new-command)
-                jump (get-jump new-command)
-                dest-bin (get dest-jump-map dest "000")
-                comp-bin (get comp-map comp "0000000")
-                jump-bin (get dest-jump-map jump "000")
-                _ (println "comp" comp "->" comp-bin
-                           ",dest" dest "->" dest-bin
-                           ",jump" jump "->" jump-bin)]
-            (conj output (str "111" comp-bin dest-bin jump-bin)))
-          :else (conj output new-command))))
-
 (defn parser
   "将输入的简单 .asm 汇编代码进行解析"
   [commands]
-  (reduce collector [] commands))
+  (reduce 
+   (fn [output new-command]
+     (let [new-command (str/trim new-command)]
+       (cond (str/blank? new-command) output
+             (str/starts-with? new-command "//") output
+             (is-a new-command)
+             (let [numstr (get-symbol new-command)]
+               (conj output (str "0" (imme-num->bin numstr))))
+             (is-c new-command)
+             (let [dest     (get-dest new-command)
+                   comp     (get-comp new-command)
+                   jump     (get-jump new-command)
+                   dest-bin (get dest-jump-map dest "000")
+                   comp-bin (get comp-map comp "0000000")
+                   jump-bin (get dest-jump-map jump "000")]
+               (conj output (str "111" comp-bin dest-bin jump-bin)))
+             :else (conj output new-command)))) 
+   [] commands))
 
 (defn read-asm
   "从文件中读入汇编程序，返回 [cmd]"
@@ -197,19 +146,19 @@
   "第一遍标记分配 ROM 地址，且将 (xxx) 伪命令映射到正确的 ROM 地址
    [[false @R0  0]] 表示是否是伪命令，当前命令（伪命令去除括号）和其地址"
   [commands]
-  (reduce (fn [result-now cmd]
-            ;(println "result is " result-now ", cmd is" cmd)
-            (let [current-is-l (is-l cmd)
-                  last-item (last result-now)
-                  last-is-l (first last-item)
-                  last-index (last last-item)
-                  current-item [current-is-l
-                                (if current-is-l (get-symbol-from-l cmd) cmd)
-                                (if (empty? result-now) 0
-                                    (if last-is-l
-                                      last-index
-                                      (+ last-index 1)))]]
-              (conj result-now current-item))) [] commands))
+  (reduce 
+   (fn [result-now cmd]
+     (let [current-is-l (is-l cmd)
+           last-item    (last result-now)
+           last-is-l    (first last-item)
+           last-index   (last last-item)
+           current-item [current-is-l
+                         (if current-is-l (get-symbol-from-l cmd) cmd)
+                         (if (empty? result-now) 0
+                             (if last-is-l
+                               last-index
+                               (+ last-index 1)))]]
+       (conj result-now current-item))) [] commands))
 
 (defn comp-asm->simple-asm
   "将复杂的 .asm 文件转换为不包含伪指令和变量的简单 .asm 指令，以供 parser 解析"
@@ -218,29 +167,26 @@
         ;解析 (xxx) 伪指令并计算其位置
         mark-fake-commands (mark-all-commands clean-commands)
         ;将预定义的符号表和上述转换得到的 (xxx) 伪指令符号合并起来，得到 符号->位置 表
-        sym-table          (merge pre-symbol-map
-                                  (into {}
-                                        (mapv #(-> % rest vec)
-                                              (filter #(-> % first) mark-fake-commands))))
+        sym-table          
+        (merge pre-symbol-map
+               (into {}
+                     (mapv #(-> % rest vec)
+                           (filter #(-> % first) mark-fake-commands))))
         sym-table          (atom sym-table)
         next-ram           (atom 16)
-        answer             (reduce (fn [agg [is-l cmd _]]
-                                     (when (str/includes? cmd "sys.init")
-                                       (println "cmd is" cmd))
-                                     (if is-l agg
-                                         (conj agg
-                        ;对于符号表存在的 @x 元素，替换，反之则从 16 开始分配
-                                               (if-let [sym (get-variable cmd)]
-                                                 (if-let [find-in-st (get @sym-table sym)]
-                                                   (do
-                                                     (when (str/includes? sym "sys.init")
-                                                       (println "sym" sym ", find! " find-in-st))
-                                                     (str "@" find-in-st))
-                                                   (let [next-addr @next-ram]
-                                                     (reset! next-ram (+ next-addr 1))
-                                                     (swap! sym-table assoc sym next-addr)
-                                                     (str "@" next-addr)))
-                                                 cmd)))) [] mark-fake-commands)]
+        answer             
+        (reduce 
+         (fn [agg [is-l cmd _]]
+           (if is-l agg
+               (conj agg
+                     (if-let [sym (get-variable cmd)]
+                       (if-let [find-in-st (get @sym-table sym)]
+                         (str "@" find-in-st)
+                         (let [next-addr @next-ram]
+                           (reset! next-ram (+ next-addr 1))
+                           (swap! sym-table assoc sym next-addr)
+                           (str "@" next-addr)))
+                       cmd)))) [] mark-fake-commands)]
     answer))
 
 
